6df7be4e915dc9c05ba22dfd2bb13b8085d4ac87
Stefan Ober <StefanO@ppedv.de>
2020-03-19 12:07:07 +0100

    SkriptVersion

--- ParameterExample.ps1
+++ ParameterExample.ps1
@@ -1,1 +1,19 @@
 ﻿
+
+$Destinationpath = "C:\testfiles2\"
+$DirCount = 2
+$FileCount = 2
+
+for($i=1;$i -le $DirCount; $i++)
+{
+    $DirName = $Destinationpath + "DIR-$("{0:D3}" -f $i)"  
+    New-Item -Path $DirName -ItemType Directory
+
+    for($j=1; $j -le $FileCount; $j ++)
+    {
+        $Filename = $DirName + "\File-$("{0:D3}" -f $j).txt"
+        New-Item -Path $Filename -ItemType File
+        Get-Date | Out-File -Append -FilePath $Filename
+    }
+}
+
