244f2f56ffae44368e79f8c9eaef4002b599ea29
Stefan Ober <StefanO@ppedv.de>
2020-03-19 14:21:15 +0100

    Skript Parametrisiert

--- ParameterExample.ps1
+++ ParameterExample.ps1
@@ -1,19 +1,59 @@
-﻿
+﻿[cmdletBinding(PositionalBinding=$false)]
+Param(
 
-$Destinationpath = "C:\testfiles2\"
-$DirCount = 2
-$FileCount = 2
+[ValidateScript({Test-Path -Path $PSItem -IsValid})]
+[Parameter(Mandatory=$true, ParameterSetName="Set1",HelpMessage="Ordner zum erstellen von Testfiles" )]
+[string]$Destinationpath,
+
+[ValidateRange(5,100)]
+[Parameter()]
+[int]$DirCount = 2,
+
+[ValidateRange(5,100)]
+[Parameter()]
+[int]$FileCount = 2,
+
+[ValidateSet("Date","ipconfig","Hostname")]
+[Parameter()]
+[string]$Filecontent = "Date",
+
+[Parameter()]
+[switch]$Clear
+
+)
+
+if((Test-Path -Path $Destinationpath) -and $Clear)
+{
+    Remove-Item -Path $Destinationpath -Force -Recurse
+}
+
+switch($Filecontent)
+{
+    "Date"     {$content = Get-Date}
+    "ipconfig" {$content = Get-NetIPConfiguration}
+    "Hostname" {$content = $env:COMPUTERNAME}
+    default    {$content = "Wrong Filecontent"}
+}
+
 
 for($i=1;$i -le $DirCount; $i++)
 {
+    if($Destinationpath.EndsWith("\") -eq $false)
+    {
+        $Destinationpath += "\"
+    }
     $DirName = $Destinationpath + "DIR-$("{0:D3}" -f $i)"  
     New-Item -Path $DirName -ItemType Directory
 
     for($j=1; $j -le $FileCount; $j ++)
     {
+        if($DirName.EndsWith("\") -eq $false)
+        {
+            $DirName += "\"
+        }
         $Filename = $DirName + "\File-$("{0:D3}" -f $j).txt"
         New-Item -Path $Filename -ItemType File
-        Get-Date | Out-File -Append -FilePath $Filename
+        Out-File -Append -FilePath $Filename -InputObject $content
     }
 }
 
