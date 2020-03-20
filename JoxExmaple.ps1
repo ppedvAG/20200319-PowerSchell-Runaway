c16d53678b47389d361906fabc810a9716407685
Stefan Ober <StefanO@ppedv.de>
2020-03-20 14:07:26 +0100

    Job Example hinzugefügt

---
+++ JoxExmaple.ps1
@@ -0,0 +1,19 @@
+﻿
+#erstellen eines Jobs
+Start-Job -Name GetTestFiles -ScriptBlock {Get-ChildItem -Path C:\Testfiles -Recurse; Start-Sleep -Seconds 25}
+
+#erhalten der JobErgebnisse der Parameter -Keep behält die daten bei die normalerweise bei Abfrage gelöscht werden
+Get-Job -Id 4 | Receive-Job 
+
+#wartet auf Background Jobs
+Wait-Job
+
+#Job Example Block mit Await
+$job = Start-Job -Name Awaitjobtext -ScriptBlock {Get-Date; Start-Sleep -Seconds 60}
+Write-Host -ForegroundColor Magenta "Verarbeite Informationen mit Skriptlogik (paralell)"
+
+$job | Wait-Job
+$ergeb = $job | Receive-Job -Keep
+
+Write-Host -ForegroundColor Green "Ergebnis des jobs" $ergeb
+
