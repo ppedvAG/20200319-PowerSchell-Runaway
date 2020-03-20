dbcfcbab38f6d0f25dbe8694482889b10acb96fb
Stefan Ober <StefanO@ppedv.de>
2020-03-20 12:00:01 +0100

    Scope Example hinzugefügt

---
+++ Scopes.ps1
@@ -0,0 +1,21 @@
+﻿
+
+function myfunction{
+[cmdletBinding()]
+param(
+    [string]$eingabe
+)
+
+    Write-Host -ForegroundColor Magenta "Functioninhalt: $eingabe"
+
+    $eingabe.ToUpper()
+    return 
+
+}
+
+$scopetest = "Vor Function"
+Write-Host -ForegroundColor Green "Scopetestinhalt: $Scopetest"
+myfunction -eingabe "FunctionText"
+Write-Host -ForegroundColor Green "Scopetestinhalt: $Scopetest"
+
+
